52839c59fc122eb0910b39f429684d04c17713fa
Szymon Nowak <szimek@gmail.com>
2014-01-24 11:07:29 +0100

    Really initial commit

---
+++ js/filedrop-peer.js
@@ -0,0 +1,132 @@
+// TODO: provide TURN server config
+
+FileDrop.Peer = function (options) {
+    this.conn = new Peer({
+        host: 'file-drop-peer-server.herokuapp.com',
+        port: 80,
+        config: {'iceServers': [
+            { url: 'stun:stun.l.google.com:19302' }
+        ]},
+        debug: 3
+    });
+
+    // When connected to PeerJS server
+    this.conn.on('open', function (id) {
+        var self = this;
+
+        $.publish('connected.server.peer', {id: id});
+        console.log('Peer:\t Connected to server with ID: ', id);
+
+        // TODO: cancel on error/disconnect
+        // Ping WebSocket server to prevent timeout on Heroku
+        window.setInterval(function () {
+            self.socket.send({type: 'ping'});
+        }, 5000);
+    });
+
+    // Listen for incoming connections
+    this.conn.on('connection', this._onConnection);
+
+    this.conn.on('close', function (error) {
+        console.log('Peer:\t Connected to server closed.');
+    });
+
+
+    this.conn.on('error', function (error) {
+        console.log('Peer:\t Error while connecting to server: ', error);
+    });
+
+    // Make sure PeerJS connection is cleaned up
+    window.onunload = window.onbeforeunload = function () {
+        if (!!this.conn && !this.conn.destroyed) {
+            this.conn.destroy();
+        }
+    };
+};
+
+FileDrop.Peer.prototype.connect = function (id) {
+    connection = this.conn.connect(id, {
+        label: 'file',
+        reliable: true
+    });
+
+    connection.on('open', function () {
+        this._onConnection(connection);
+    }.bind(this));
+
+    connection.on('error', function (error) {
+        console.log('Peer:\t P2P connection error', error);
+    }.bind(this));
+};
+
+FileDrop.Peer.prototype._onConnection = function (connection) {
+    $.publish('connected.p2p.peer', {connection: connection});
+    console.log('Peer:\t P2P connection opened: ', connection);
+
+    connection.on('data', function (data) {
+        switch (data.type) {
+        case 'info':
+            $.publish('info.p2p.peer', {
+                connection: connection,
+                info: data.payload
+            });
+            console.log('Peer:\t File info: ', data);
+            break;
+
+        case 'response':
+            $.publish('response.p2p.peer', {
+                connection: connection,
+                response: data.payload
+            });
+            console.log('Peer:\t File response: ', data);
+            break;
+
+        case 'file':
+            $.publish('file.p2p.peer', {
+                connection: connection,
+                file: data.payload
+            });
+            console.log('Peer:\t File: ', data);
+            break;
+        }
+    });
+
+    connection.on('close', function () {
+        $.publish('disconnected.p2p.peer', {connection: connection});
+        console.log('Peer:\t P2P connection opened: ', connection);
+    });
+};
+
+FileDrop.Peer.prototype.sendFileInfo = function (connection, file) {
+    var info = {
+        lastModifiedDate: file.lastModifiedDate,
+        name: file.name,
+        size: file.size,
+        type: file.type
+    };
+
+    connection.send({
+        type: 'info',
+        payload: info
+    });
+};
+
+FileDrop.Peer.prototype.sendFileResponse = function (connection, response) {
+    connection.send({
+        type: 'response',
+        payload: response
+    });
+};
+
+FileDrop.Peer.prototype.sendFile = function (connection, file) {
+    connection.send({
+        type: 'file',
+        payload: {
+            lastModifiedDate: file.lastModifiedDate,
+            name: file.name,
+            size: file.size,
+            type: file.type,
+            data: file
+        }
+    });
+};
---
+++ js/filedrop-room.js
@@ -0,0 +1,75 @@
+FileDrop.Room = function () {
+    var url = window.location.protocol + '//' + window.location.hostname;
+    this._socket = new io.connect(url);
+    this.name = null;
+};
+
+FileDrop.Room.prototype.join = function (user) {
+    var self = this;
+
+    // Get room name
+    xhr = $.getJSON('/room')
+
+    // Join room and listen for changes
+    .then(function (data) {
+        var socket = self._socket;
+
+        self.name = data.name,
+
+        $.extend(user, {uuid: data.uuid});
+
+        socket.emit('join', {
+            room: self.name,
+            peer: user
+        });
+        console.log('Room:\t Connecting to: ', self.name);
+
+        socket.on('user_list', function (data) {
+            console.log('Room:\t Connected to: ', self.name);
+            $.publish('connected.room', user);
+
+            console.log('Room:\t user_list: ', data);
+            $.publish('user_list.room', [data]);
+        });
+
+        socket.on('user_added', function (user) {
+            console.log('Room:\t user_added: ', user);
+            $.publish('user_added.room', user);
+        });
+
+        socket.on('user_changed', function (user) {
+            console.log('Room:\t user_changed: ', user);
+            $.publish('user_changed.room', user);
+        });
+
+        socket.on('user_removed', function (user) {
+            console.log('Room:\t user_removed: ', user);
+            $.publish('user_removed.room', user);
+        });
+
+        socket.on('disconnect', function () {
+            console.log('Room:\t disconnect');
+        });
+
+        socket.on('error', function () {
+            console.log('Room:\t error');
+        });
+
+        socket.on('reconnecting', function () {
+            console.log('Room:\t reconnecting');
+        });
+
+        socket.on('reconnect', function () {
+            console.log('Room:\t reconnect');
+        });
+    });
+
+    return this;
+};
+
+FileDrop.Room.prototype.update = function (attrs) {
+    this._socket.emit('update', {
+        room: this.name,
+        peer: attrs
+    });
+};
