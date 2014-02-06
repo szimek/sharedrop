8858b9e8ce958b051939ad4c95c8c2027c63669f
Szymon Nowak <szimek@gmail.com>
2014-02-06 15:09:12 +0100

    Fix resending same file after clicking on peer avatar and canceling transfer

--- app/scripts/app/views/file_field.js
+++ app/scripts/app/views/file_field.js
@@ -12,5 +12,16 @@ FileDrop.FileField = Ember.TextField.extend({
             file = files[0];
 
         this.sendAction('action', { file: file });
-    }
+    },
+
+    // Hackish way to reset file input when sender cancels file transfer,
+    // so if sender wants later to send the same file again,
+    // the 'change' event is triggered correctly.
+    fileDidChange: function () {
+        if (!this.get('file')) {
+            var field = this.$();
+            field.wrap('<form>').closest('form').get(0).reset();
+            field.unwrap();
+        }
+    }.observes('file')
 });
