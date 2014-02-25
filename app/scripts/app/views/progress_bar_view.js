9046bf6c7f842440449706a685a5d4c159b84e0b
Maciej Jarczok <maciej.jarczok@gmail.com>
2014-02-26 00:27:53 +0100

    fix svg progress bar styles

--- app/scripts/app/views/progress_bar_view.js
+++ app/scripts/app/views/progress_bar_view.js
@@ -3,9 +3,9 @@ FileDrop.ProgressBarView = Ember.View.extend({
     templateName: 'progress-bar',
 
     attributeBindings: ['width', 'height', 'viewport'],
-    width: "84",
-    height: "84",
-    viewport: "0 0 84 84",
+    width: "74",
+    height: "74",
+    viewport: "0 0 74 74",
 
     didInsertElement: function () {
         this.set('path', this.$().find('path'));
@@ -28,9 +28,9 @@ FileDrop.ProgressBarView = Ember.View.extend({
             α = progress * 360,
             r = ( α * π / 180 ),
             mid = ( α > 180 ) ? 1 : 0,
-            x = Math.sin( r ) * 42,
-            y = Math.cos( r ) * - 42,
-            anim = 'M 0 0 v -42 A 42 42 1 '
+            x = Math.sin( r ) * 37,
+            y = Math.cos( r ) * - 37,
+            anim = 'M 0 0 v -37 A 37 37 1 '
                  + mid + ' 1 '
                  +  x  + ' '
                  +  y  + ' z';
