ca1764215a97f6642401cbe442250924a023c03a
Szymon Nowak <szimek@gmail.com>
2014-03-07 20:30:30 +0100

    Display alerts (for now) if there's no WebRTC or FileSystem fails

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,21 @@
 window.FileDrop.App = Ember.Application.create();
 
-// Clear HTML5 filesystem on page load
+// Test for browser support
 FileDrop.App.deferReadiness();
-FileDrop.File.removeAll().then(function () {
+if (!(('webkitRTCPeerConnection' in window) && util.supports.sctp)) {
+    alert("Your browser is not supported");
+} else {
+    FileDrop.App.advanceReadiness();
+}
+
+// Try to clear HTML5 filesystem on page load
+// If it fails, it might mean that incognito mode is used
+FileDrop.App.deferReadiness();
+FileDrop.File.removeAll()
+.then(function () {
     console.log("Cleared HTML5 filesystem");
     FileDrop.App.advanceReadiness();
+})
+.catch(function (error) {
+    alert("This app doesn't work in incognito mode");
 });
