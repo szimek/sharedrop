190e1cfe79105e23162081c80ed4cf9030c404fe
Szymon Nowak <szimek@gmail.com>
2014-04-06 19:48:39 +0200

    Add initial version of select for local IPs

--- app/scripts/app/views/user_view.js
+++ app/scripts/app/views/user_view.js
@@ -1,5 +1,10 @@
 ShareDrop.App.UserView = Ember.View.extend({
-    isConnected: Ember.computed.alias('controller.model.isConnected'),
     classNames: ['peer'],
-    classNameBindings: ['isConnected:connected:disconnected']
+    classNameBindings: ['isConnected:connected:disconnected'],
+    isConnected: Ember.computed.alias('controller.model.isConnected'),
+    localIps: function () {
+        // Convert unordered set to sorted array
+        var ips = this.get('controller.model.local_ips');
+        return ips.toArray().sort();
+    }.property('controller.model.local_ips.[]')
 });
