52370eb7ba48ae8e601dec8ad46e4b1e5b614c95
Szymon Nowak <szimek@gmail.com>
2014-04-13 19:59:05 +0200

    Display select for local IPs only if there's more than one

--- app/scripts/app/views/user_view.js
+++ app/scripts/app/views/user_view.js
@@ -2,9 +2,14 @@ ShareDrop.App.UserView = Ember.View.extend({
     classNames: ['peer'],
     classNameBindings: ['isConnected:connected:disconnected'],
     isConnected: Ember.computed.alias('controller.model.isConnected'),
+
     localIps: function () {
         // Convert unordered set to sorted array
         var ips = this.get('controller.model.local_ips');
         return ips.toArray().sort();
-    }.property('controller.model.local_ips.[]')
+    }.property('controller.model.local_ips.[]'),
+
+    hasManyLocalIps: function () {
+        return this.get('localIps.length') > 1;
+    }.property('localIps.length')
 });
