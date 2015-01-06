31cec1202b3b98595cb8a9362afd4f942e2ae36a
Szymon Nowak <szimek@gmail.com>
2015-01-06 15:29:43 +0100

    Try to make FIREBASE_URL available in Ember CLI app

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,9 @@ module.exports = function (environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    FIREBASE_URL: process.env.FIREBASE_URL
   };
 
   if (environment === 'development') {
