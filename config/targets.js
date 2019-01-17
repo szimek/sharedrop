e58a9aa3c1b4b2e3f20b496463e6013a36d07754
Szymon Nowak <szimek@gmail.com>
2019-01-17 10:40:20 +0100

    Add browser targets for Babel

---
+++ config/targets.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const browsers = [
+  'last 2 Chrome versions',
+  'last 2 Firefox versions',
+  'last 1 Safari versions',
+  'last 1 Edge versions',
+];
+
+module.exports = {
+  browsers,
+};
