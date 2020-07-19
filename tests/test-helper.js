b6648e73fd14f4a655ce158380d9ee53bbe71c7b
Szymon Nowak <szimek@gmail.com>
2020-07-19 22:03:44 +0200

    Fix running tests, even though there are none...

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,10 @@
 /* eslint */
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import Application from '../app/app';
 // eslint-disable-next-line import/extensions
-import config from './config/environment';
+import Application from '../app';
+// eslint-disable-next-line import/extensions
+import config from '../config/environment';
 
 setApplication(Application.create(config.APP));
 
