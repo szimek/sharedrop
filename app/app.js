52aeb41f9c32b272b4afc8f36fa7d68f2ebff6fd
Szymon Nowak <szimek@gmail.com>
2020-09-25 19:09:44 +0200

    Add Sentry script

--- app/app.js
+++ app/app.js
@@ -2,6 +2,14 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'sharedrop/config/environment';
+import * as Sentry from '@sentry/browser';
+import { Ember as EmberIntegration } from '@sentry/integrations';
+
+Sentry.init({
+  dsn:
+    'https://ba1292a9c759401dbbda4272f183408d@o432021.ingest.sentry.io/5384091',
+  integrations: [new EmberIntegration()],
+});
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
